7b17a67fee6c5964b3315234059f5217366319e7
marh0819 <miguel.rivera01@uceva.edu.co>
2023-04-19 11:37:01 -0500

    Initial commit

---
+++ src/main/java/co/edu/uceva/facultad_service/FacultadServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.facultad_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacultadServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uceva/facultad_service/FacultadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.facultad_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
