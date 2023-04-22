bca794f6a161b9c5fddbff1624b876bf7811af40
Miguel Angel Rivera Henao <miguel.rivera01@uceva.edu.co>
2023-04-22 10:50:52 -0500

    Se creo la entidad Facultad

--- src/main/java/co/edu/uceva/facultad_service/model/entities/Facultad.java
+++ src/main/java/co/edu/uceva/facultad_service/model/entities/Facultad.java
@@ -1,21 +1,22 @@
 package co.edu.uceva.facultad_service.model.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "facultad")
 public class Facultad {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_facultad")
     private Long id;
 
     private String nombre;
